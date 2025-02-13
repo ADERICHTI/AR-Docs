34ce699e3967913063a6334b320c509b1bfb5fe9
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-02-13 14:41:40 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -4,4 +4,13 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
-python manage.py createsuperuser
+SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@example.com"}
+SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD:-"adminpassword"}
+SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME:-"admin"}
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='${SUPERUSER_USERNAME}').exists():
+    User.objects.create_superuser('${SUPERUSER_USERNAME}', '${SUPERUSER_EMAIL}', '${SUPERUSER_PASSWORD}')
+EOF
